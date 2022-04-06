d28716515f4b5c6bc82fc766682b58474f26472b
cameronhoehne <98066436+cameronhoehne@users.noreply.github.com>
2022-04-06 16:45:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,154 @@
+//Congigure autocomplete specifically for comparing movie data
+
+const autoCompleteConfig = {
+renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+     return `
+     <img src="${imgSrc}"/>
+     ${movie.Title} (${movie.Year})
+     `;
+},
+    inputValue(movie) {
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get("http://www.omdbapi.com/", {
+            params: {
+                apikey: "d34662ac",
+                s: searchTerm
+            }
+        });
+            //Handle API error
+            if (response.data.Error) {
+                return [];
+            }
+            //Return data
+            console.log(response.data)
+            return response.data.Search;
+    }
+}
+
+//Generate two dropdowns
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector("#left-autocomplete"),
+    onOptionSelect(movie) {
+        document.querySelector(".tutorial").classList.add("is-hidden")
+        onMovieSelect(movie, document.querySelector("#left-summary"), "left");
+    }
+});
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector("#right-autocomplete"),
+    onOptionSelect(movie) {
+        document.querySelector(".tutorial").classList.add("is-hidden")
+        onMovieSelect(movie, document.querySelector("#right-summary"), "right");
+    }
+});
+
+
+//Sends followup request to get more information after movie is selected
+
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "d34662ac",
+            i: movie.imdbID
+        }
+    });
+    summaryElement.innerHTML = movieTemplate(response.data);
+    
+    //Checking if sides have been defined - if not, it defines them
+  
+    if (side === "left") {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+};
+
+//Function to compare movies once they are defined
+
+const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary .notification')
+    const rightSideStats = document.querySelectorAll('#right-summary .notification')
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary')
+            leftStat.classList.add('is-danger')
+        } else {
+            rightStat.classList.remove('is-primary')
+            rightStat.classList.add('is-danger')
+        }
+    })
+}
+
+//Generates HTML of the detailed movie information
+
+const movieTemplate = (movieDetail) => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, ""))
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""))
+    //Extracts integers from "awards" and sums them together
+    const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+        const value = parseInt(word);
+        if (isNaN(value)) {
+            return prev;
+        } else {
+            return prev + value;
+        }
+    }, 0)
+    
+    //Generating the HMTL
+    return `
+    <article class="media">
+        <figure class="media=left">
+            <p class"image">
+                <img src="${movieDetail.Poster}"/>
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetail.Title}</h1>
+                <h4>${movieDetail.Genre}</h4>
+                <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article>
+    <article data-value="${awards}" class="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>  
+    </article>
+    <article data-value="${dollars}" class="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>  
+    </article>
+    <article data-value="${metascore}" class="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>  
+    </article>
+    <article data-value="${imdbRating}" class="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>  
+    </article>
+    <article data-value="${imdbVotes}" class="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>  
+    </article>
+    `
+}
---
+++ utils.js
@@ -0,0 +1,13 @@
+//Delay for user input send
+
+const debounce = (func, delay = 1000) => {
+    let timeOutId;
+    return (...args) => {
+        if (timeOutId) {
+            clearTimeout(timeOutId)
+        }
+        timeOutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
